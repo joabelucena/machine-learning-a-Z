23df47418d792a9da5245c562fdcfc45c44a4f68
Joabe Lucena <joabelucena@gmail.com>
2018-02-17 18:38:56 -0200

    P2:S8 - Decision Tree (Python)

--- Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression_jobs.py
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/decision_tree_regression_jobs.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 14 20:53:04 2018
+Created on Sat Feb 17 18:00:36 2018
 
 @author: Joabe
 """
 
-# Regression Template
+# Decision Tree Decision Tree 
 
-# Polynomial Regression
+# Decision Tree  Template
+
+# Polynomial Decision Tree 
 #%%
 # Importing the libraries
 import numpy as np
@@ -18,7 +20,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Position_salaries.csv')
 X = dataset.iloc[:, 1:2].values # 1:2 for X be considered as a matrix
-y = dataset.iloc[:, -1].values
+y = dataset.iloc[:, -1:].values
 
 #%%
 # Splitting the dataset into the Training set and Test set
@@ -36,31 +38,42 @@ sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
 #%%
-## Fitting the Regression to the dataset
-#Create your regressor here
+## Fitting the Decision Tree  to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
 
 
 #%%
-## Predict a new result with Regression
+## Predict a new result with Decision Tree 
 y_pred = regressor.predict(6.5)
 
 
 #%%
-## Visualising the Regression results
+## Visualising the Decision Tree Regression results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
+plt.title('Truth or Bluff (Decision Tree Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
 #%%
-## Visualising the Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
+## Visualising the Decision Tree Regression results (for higher resolution and smoother curve)
+
+###  ATENTION POINT ###
+
+# For non-continuous models (which is the case of CART), we must use a higher resolution grid than 
+# vthe original grid used to train the model. This is necessary becaus as we know, decision tree splits
+# the dataset in n regions and the Y predicted point is gonna be the average of the observations contained
+# in this region. So, if you plot a chart predicting original X vector, the predicted points is gonna be
+# exactly the same as the observed points.
+
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape(len(X_grid), 1) #reshapes the array into a matrix
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Regression Model)')
+plt.title('Truth or Bluff (Decision Tree Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
